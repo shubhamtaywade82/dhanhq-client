6ada2c7ca643b70291c6b2bbd732b5ba92bbae7b
Shubham Taywade <shubhamtaywade82@gmail.com>
2025-01-28 15:05:20 +0530

    Refactor OptionChain resource to use new API endpoints for fetching option chain and expiry list data; add corresponding tests

--- lib/DhanHQ/resources/option_chain.rb
+++ lib/DhanHQ/resources/option_chain.rb
@@ -3,14 +3,22 @@
 module DhanHQ
   module Resources
     class OptionChain < BaseAPI
-      HTTP_PATH = "/optionchain"
+      HTTP_PATH = "/v2"
 
-      def fetch(params)
-        post("", params: params)
+      # Fetch the Option Chain for an underlying
+      #
+      # @param params [Hash] Parameters including `UnderlyingScrip`, `UnderlyingSeg`, and `Expiry`
+      # @return [Hash] The API response with the option chain data
+      def fetch_option_chain(params)
+        post("/optionchain", params: params)
       end
 
-      def expiry_list(params)
-        post("/expirylist", params: params)
+      # Fetch the expiry list for options of an underlying
+      #
+      # @param params [Hash] Parameters including `UnderlyingScrip` and `UnderlyingSeg`
+      # @return [Array<String>] List of expiry dates
+      def fetch_expiry_list(params)
+        post("/optionchain/expirylist", params: params)
       end
     end
   end
---
+++ spec/dhan_hq/resources/option_chain_spec.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+RSpec.describe DhanHQ::Resources::OptionChain do
+  let(:option_chain) { described_class.new }
+
+  let(:option_chain_params) do
+    {
+      UnderlyingScrip: 13,
+      UnderlyingSeg: "IDX_I",
+      Expiry: "2025-01-30"
+    }
+  end
+
+  let(:expiry_list_params) do
+    {
+      UnderlyingScrip: 13,
+      UnderlyingSeg: "IDX_I"
+    }
+  end
+
+  before do
+    DhanHQ.configure_with_env
+  end
+
+  describe "#fetch_option_chain" do
+    it "fetches the option chain data successfully", :vcr do
+      VCR.use_cassette("resources/option_chain") do
+        response = option_chain.fetch_option_chain(option_chain_params)
+        expect(response["data"]["last_price"]).to eq(22952.94921875)
+        expect(response["data"]["oc"]["22900.000000"]["ce"]["last_price"]).to eq(186.2)
+        expect(response["data"]["oc"]["22900.000000"]["pe"]["last_price"]).to eq(105.4)
+      end
+    end
+  end
+
+  describe "#fetch_expiry_list" do
+    it "fetches the expiry list successfully", :vcr do
+      VCR.use_cassette("resources/expiry_list") do
+        response = option_chain.fetch_expiry_list(expiry_list_params)
+        expect(response["data"]).to include("2025-01-30", "2025-02-13")
+      end
+    end
+  end
+end
